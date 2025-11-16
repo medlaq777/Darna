776c2a195c89723292bc13dc24b9618e8ad9a888
medlaq777 <m.laqsoumi@gmail.com>
2025-11-16 22:04:34 +0100

    Create The Auth middleware and also fix issues in router also install keycloak to implemenent sso Oauth2

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,22 @@
+import Jwt from "../utils/jwt.util.js";
+
+class AuthMiddleware {
+  static protect(req, res, next) {
+    try {
+      const header = req.headers.authorization;
+      if (!header?.startsWith("Bearer ")) {
+        const err = new Error("Unauthorized");
+        err.status = 401;
+        throw err;
+      }
+      const token = header.split(" ")[1];
+      const payload = Jwt.verifyToken(token);
+      req.user = payload;
+      next();
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+export default AuthMiddleware;
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,12 +1,17 @@
 import express from "express";
 import authController from "../controllers/auth.controller.js";
+import AuthMiddleware from "../middlewares/auth.middleware.js";
 
 class AuthRoute {
   static build() {
     const router = express.Router();
     router.post("/register", authController.register.bind(authController));
     router.post("/login", authController.login.bind(authController));
-    router.get("/profile", authController.profile.bind(authController));
+    router.get(
+      "/profile",
+      AuthMiddleware.protect,
+      authController.profile.bind(authController)
+    );
     return router;
   }
 }
